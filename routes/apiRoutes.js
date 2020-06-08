bf8ce61529f08d979ed365dddce2ecc0a6c066af
Lala <lalagg@hotmail.com>
2020-06-08 19:40:31 -0400

    removed js and css folders

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 
-//add routes
+//add routes:
+//todo: route for saving activity data
+// todo: route for getting activity data by duration
+// TODO: route for getting activity data by mileage
 
 // router.get
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 
 //add routes
 
+// HTML route to display page
+
 // router.get
 
 // router.post
