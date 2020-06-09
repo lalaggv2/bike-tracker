a110630ed57f24add164ff0a3d1efc631532cb33
Lala <lalagg@hotmail.com>
2020-06-08 20:09:43 -0400

    html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
-
+const path = require("path");
 //add routes
 
 // HTML route to display page
+
+router.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "..", "public", "html", "index.html");
+  res.sendFile(filePath);
+});
 
 // router.get
 
